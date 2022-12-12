cf3f58287962708f632359858fa7813ebf6ee9fb
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-12 16:16:40 -0500

    MealItemForm: implementing the Input component with all its props

--- src/components/Meals/Mealitem/MealitemForm.js
+++ src/components/Meals/Mealitem/MealitemForm.js
@@ -1,12 +1,24 @@
+import React, { Fragment } from 'react';
+
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
 return (
-  <form className={classes.form}>
-    <input type="text" />
-    <button>+ Add</button>
-  </form>
-)
+    <Fragment>
+      <form className={classes.form}>
+        <Input label='Amount' input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }} />
+        <button>+ Add</button>
+      </form>
+    </Fragment>
+  )
 }
 
 export default MealItemForm;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,8 +4,8 @@ const Input = (props) => {
 return (
   <div className={classes.input}>
     <label htmlFor={props.input.id}>{props.label}</label>
-    <input id={props.input.id} {...props.input} />
-    {/*{...props.input} = means whatever type onf input is decided will be rendered */}
+    <input {...props.input} />
+    {/*{...props.input} = means it's configurable from oustide the component whatever type onf input is decided will be rendered */}
   </div>
 
 )
