42037004300bbc50de4792a54c52aa46f145809e
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-14 15:22:58 -0500

    Modal: installing basics + 2 components within the component

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,28 @@
+import { Fragment } from 'react';
+
 import classes from './Modal.module.css';
 
-const Modal = (props) => {};
+const Backdrop = (props) => {
+  return (
+    <div className={classes.backdrop} />
+  );
+};
+
+const Overlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      <Backdrop />
+      <Overlay />
+    </Fragment>
+  );
+};
 
 export default Modal;
