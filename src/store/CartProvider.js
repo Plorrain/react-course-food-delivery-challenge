a63505f682a9c6f5b99d8fe826b13f6ddad6fc73
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-15 14:36:58 -0500

    App.js: linking cartprovider and wrapping jsx with it

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,21 +2,21 @@ import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {};
+
   const removeItemFromCartHandler = (id) => {};
 
   const cartContext = {
     items: [],
     totalAmount: 0,
-    addItem: (item) => {},
-    removeItem: (id) => {}
-
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
   };
 
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
-  )
+  );
 };
 
 export default CartProvider;
